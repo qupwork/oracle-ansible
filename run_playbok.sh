e193a152c27d49400f7ea57733a622735eb3ca9a
qdnqn <adnn.selimovic@gmail.com>
2024-07-19 08:25:53 +0200

    Fix deploy playbook

---
+++ run_playbok.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd oracle-ansible
+git pull
+cd ..
+
+ansible-playbook -i ../inventory.yaml cleanup_containers_playbook.yaml --extra-vars "container_registry_username=adnn.selimovic@gmail.com autonomous_database_tag=v0.0.1 weblogic_server_tag=v0.0.1 weblogic_username=hello weblogic_password=123456789"
+
+ansible-playbook -i ../inventory.yaml build_deploy_containers_playbook.yaml --extra-vars "weblogic_username=hello weblogic_password=123456789 autonomous_database_tag=v0.0.1 weblogic_server_tag=v0.0.1"
+
